499e6e79f7908db0cebd8a8145ce507298c6f997
CdMnpltr <74214328+CdMnpltr@users.noreply.github.com>
2024-05-10 23:30:08 -0700

    PseudoCoding random value generator

---
+++ rps.js
@@ -0,0 +1,34 @@
+console.log('Hello');
+
+/*PseudoCode 
+
+Block 1
+Create function getComputerChoice
+    - within, create code that returns random string: 'rock', 'paper', 'scissors' (can use Math.random)
+
+    -- code below --
+    
+    function getComputerChoice (rock, paper, scissor) {
+
+    }
+
+    
+
+    (Conditional: multiply Mathrandom by 100 then add 1 and create conditional (||?) that falls between 0-32(rock), 33-62(paper), 63-99(scissor)). 
+
+        - create index with rock paper scissors as value (choices)
+        - use mathRandom to pick out random value from index
+
+    -- code below
+
+    const choices = [rock, paper, scissors];
+
+    
+
+    // concat code below
+
+
+Block 2 (test block 1 before continuing...)
+
+*/
+
